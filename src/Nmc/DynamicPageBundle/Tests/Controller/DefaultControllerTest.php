e7fc6b7153287fa1095f6393017edbf368a3d336
nikoms <nikoms@gmail.com>
2014-08-15 17:40:21 +0200

    Load fixture in test setUp

--- src/Nmc/DynamicPageBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Nmc/DynamicPageBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,10 +2,18 @@
 
 namespace Nmc\DynamicPageBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+        $classes = array(
+            'Nmc\DynamicPageBundle\DataFixtures\ORM\LoadWebsiteData',
+        );
+        $this->loadFixtures($classes);
+    }
     public function testIndex()
     {
         $client = static::createClient(array(),array('HTTP_HOST' => 'fmip.git'));
